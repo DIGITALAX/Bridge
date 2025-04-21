43f20950ffd4c6e5eee9ce64687620ee18a12016
emmajane1313 <emma.jane@digitalax.xyz>
2025-04-21 15:17:57 -0400

    metadata

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,8 @@
 import { MetadataRoute } from "next";
+import { LOCALES } from "./lib/constants";
 
-export default async function sitemap({
-  params,
-}: {
-  params: { lang: string };
-}): Promise<MetadataRoute.Sitemap> {
-  return [
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/${params.lang}`,
-    },
-  ];
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return LOCALES.map((lang) => ({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/${lang}`,
+  }));
 }
